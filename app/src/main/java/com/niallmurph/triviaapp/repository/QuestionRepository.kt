158ace06c1e88b48d9d8773d0d17b3dbd80ac73c
Niall <niall@Aarons-MacBook-Air.local>
2024-01-15 09:03:12 +0000

    Added getAllQuestions wrapper to repository

--- app/src/main/java/com/niallmurph/triviaapp/repository/QuestionRepository.kt
+++ app/src/main/java/com/niallmurph/triviaapp/repository/QuestionRepository.kt
@@ -1,10 +1,24 @@
 package com.niallmurph.triviaapp.repository
 
+import android.util.Log
 import com.niallmurph.triviaapp.data.DataOrException
 import com.niallmurph.triviaapp.model.QuestionItem
 import com.niallmurph.triviaapp.network.QuestionApi
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionApi) {
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if(dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Data Exception", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+        return dataOrException
+    }
 }
